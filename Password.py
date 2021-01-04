87f5912dbedc72eab61f890e5828dc72d984cea0
Aaron L. Hevia <aaron.hevia@gmail.com>
2021-01-04 13:02:40 -0500

    Edited character list and Created CreatePWD function.

--- Password.py
+++ Password.py
@@ -1,9 +1,24 @@
+import getpass
+
 characters = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', 
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')'
 ]
 
+#Shuffle characters to create Individual Account Cipher.
+
 #Create and Verify Password.
+def CreatePWD():
+  while True:
+    password = getpass.getpass("Enter a Password:  ")
+    verify = getpass.getpass("Retype your Password:  ")
+    if password != verify:
+      print("Invalid entry.")
+    else:
+      break
+
+  return password
 
 #Create and Verify PIN.
 
