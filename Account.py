4d259d95cf4c0ef1e070398ea3b78d7bb8998be3
Aaron L. Hevia <aaron.hevia@gmail.com>
2021-01-09 15:13:52 -0500

    Updated Account class syntax

--- Account.py
+++ Account.py
@@ -1,19 +1,18 @@
 class Account:
 
-    def __init__():
+    def __init__(self):
         pass
 
-    def AddNewPlayer(activePlayers, name, pwd, pin, characters):
+    def AddNewPlayer(self, activePlayers, name, pwd, pin, characters):
         # New players will start with $1000, $$ Won, $$ Lost, Ciphered Password, PIN, and Shuffled Alphabet    
         player  = activePlayers[name] = [1000, 0, 0, password, pin, characters]
         return player
 
-    def NewPlayer(activePlayers, retiredPlayers):    
+    def NewPlayer(self, activePlayers, retiredPlayers):    
         allPlayers = activePlayers | retiredPlayers    
-        name = input("\nEnter a player name:  ")
-        i = len(allPlayers)
+        name = input("\nEnter a player name:  ")        
 
-        if i == 0:        
+        if len(allPlayers) == 0:        
             #TODO:  Password, PIN and Characters creation.
             pwd = ""    #TODO:  Get password from Password Class.
             pin = 0 #TODO: Get "PIN" (Cipher) from Password Class.
@@ -22,24 +21,24 @@ class Account:
             for player in allPlayers:
                 if name == player:
                     print("Player already exists in session.  Enter a new player.")
-                    NewPlayer(activePlayers, retiredPlayers)
+                    self.NewPlayer(activePlayers, retiredPlayers)
                 else:                
                     #TODO:  Password, PIN and Characters creation.
                     pwd = ""    #TODO:  Get password from Password Class.
                     pin = 0 #TODO: Get "PIN" (Cipher) from Password Class.
                     characters = [] #TODO: Get shuffled characters from Password Class.
             
-        AddNewPlayer(activePlayers, name, password, pin, characters)
+        self.AddNewPlayer(activePlayers, name, password, pin, characters)
         return activePlayers
 
-    def RetirePlayer(activePlayers, retiredPlayers):
+    def RetirePlayer(self, activePlayers, retiredPlayers):
         name = input("Which player will be retiring:  ")
         #TODO:  Validate account to retire.
         retiredPlayers[name] = activePlayers[name]    
         del activePlayers[name]
         print(f"{name} has retired with ${retiredPlayers[name][0]}.  You have made ${retiredPlayers[name][1]} and lost ${retiredPlayers[name][2]} in this session.")
 
-    def UnretirePlayer(activePlayers, retiredPlayers):
+    def UnretirePlayer(self, activePlayers, retiredPlayers):
         name = input("Which player will be returning to the table:  ")
         #TODO:  Validate account to return.
         activePlayers[name] = retiredPlayers[name]
